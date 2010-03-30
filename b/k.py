c7b176ff2cd2eb48227f200f802a6b8b33352907
dchichkov <dchichkov@8cfbf50e-4c19-8517-648c-a395f638bb4c>
2010-03-30 09:20:59 +0000

    +internal: in progress.

--- b/k.py
+++ b/k.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+
+
 from collections import defaultdict
 from bisect import bisect
 from pprint import pprint
@@ -33,12 +37,16 @@ def is_known_as_verified(id):
         if in_sorted_list(l, id): return k
     return None
 
-
 def is_known_as_good_or_bad(id):
     if in_sorted_list(ids['good'], id): return 'good'
     if in_sorted_list(ids['bad'], id): return 'bad'
     return None
 
+
+def is_verified_or_known_as_good_or_bad(id):
+    verified = is_known_as_verified(id)
+    if(verified): return ('good', 'bad')[verified[:3] == 'bad']
+    return is_known_as_good_or_bad(id)
 
 
 
@@ -82,17 +90,37 @@ if __name__ == "__main__":
                         self.fail()
                     elif verified and known == 'good':
                         if verified.find('good') < 0 and verified.find('constructive') < 0:
+                            print "Warning(c) at k = %s id = %s, verified = %s, known = %s" % (k, id, verified, known)
+                    elif verified and known:
+                        if verified.find(known) < 0:
+                            print "Warning(d) at k = %s id = %s, verified = %s, known = %s" % (k, id, verified, known)
+                    test_cases += 1
+            print "(%d)" % test_cases
+
+        def test_is_verified_or_known_as_good_or_bad(self):
+            """verify that is_verified_or_known_as_good_or_bad works and check dataset for errors"""
+            test_cases = 0
+            for k, l in ids.items():
+                for id in l:
+                    verified = is_known_as_verified(id)
+                    known = is_verified_or_known_as_good_or_bad(id)
+                    if verified and not known:
+                        print "FAILED(c) at k = %s id = %s, verified = %s, known = %s" % (k, id, verified, known)
+                        self.fail()
+                    elif verified and known == 'good':
+                        if verified.find('good') < 0 and verified.find('constructive') < 0:
                             print "FAILED(c) at k = %s id = %s, verified = %s, known = %s" % (k, id, verified, known)
                             self.fail()
                     elif verified and known:
                         if verified.find(known) < 0:
-                            print "FAILED(c) at k = %s id = %s, verified = %s, known = %s" % (k, id, verified, known)
+                            print "FAILED(d) at k = %s id = %s, verified = %s, known = %s" % (k, id, verified, known)
                             self.fail()
                     test_cases += 1
             print "(%d)" % test_cases
 
+
         def test_duplicates(self):
-            """verify that lists don't have any duplicates"""
+            """verify that lists don't have any duplicates and sorted in the correct order"""
             test_cases = 0
             for k, l in ids.items():
                 prev = 0
