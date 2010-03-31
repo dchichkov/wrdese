1dbe4a0c8143322b82f96f3fe4ee76724a05c743
dchichkov <dchichkov@8cfbf50e-4c19-8517-648c-a395f638bb4c>
2010-03-31 07:11:42 +0000

    +internal: in progress.

--- trunk/b/crm114.py
+++ trunk/b/crm114.py
@@ -67,8 +67,8 @@ kCrmPath = "crm"
 kClassificationType = "<osb unique microgroom>"
 kClassificationExtension = ".css"
 
-#kClassificationType = "<hyperspace>"
-#kClassificationExtension = ".css"
+# kClassificationType = "<hyperspace>"
+# kClassificationExtension = ".css"
 
 #kClassificationType = "<osb unigram>"
 #kClassificationExtension = ".css"
