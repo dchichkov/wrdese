de7388765a5bd7db71597b0a99376a4da10389f2
dchichkov@gmail.com <dchichkov@gmail.com@8cfbf50e-4c19-8517-648c-a395f638bb4c>
2010-05-10 09:05:25 +0000

    *alpha release

--- trunk/b/ircbot.py
+++ trunk/b/ircbot.py
@@ -8,59 +8,113 @@
 # Joel Rosdahl <joel@rosdahl.net>
 
 import irclib
-import sys
+import sys, re
+import r
 
-class IRCCat(irclib.SimpleIRCClient):
-    def __init__(self, target):
-        irclib.SimpleIRCClient.__init__(self)
-        self.target = target
 
-    def on_welcome(self, connection, event):
-        if irclib.is_channel(self.target):
-            connection.join(self.target)
+def on_connect(connection, event):
+    if connection == w:
+        print "Welcomed on irc.wikimedia.org"
+        if irclib.is_channel("#en.wikipedia"):
+            connection.join("#en.wikipedia")
         else:
-            self.send_it()
+            print "Error: #en.wikipedia is not a channel. "
+            sys.exit(0)
+            
+    if connection == c:            
+        print "Welcomed on irc.freenode.net"
+        connection.join("#cvn-wp-en-reputation")        
+    
+def on_join(connection, event):
+    print ("Joined:", event.arguments() [ 0 ])
 
-    def on_join(self, connection, event):
-        self.send_it()
-
-    def on_disconnect(self, connection, event):
-        sys.exit(0)
-
-    def send_it(self):
-        while 1:
-            line = sys.stdin.readline()
-            if not line:
-                break
-            self.connection.privmsg(self.target, line)
-        self.connection.quit("Using irclib.py")
-
-def main():
-    if len(sys.argv) != 4:
-        print "Usage: irccat2 <server[:port]> <nickname> <target>"
-        print "\ntarget is a nickname or a channel."
-        sys.exit(1)
-
-    s = sys.argv[1].split(":", 1)
-    server = s[0]
-    if len(s) == 2:
-        try:
-            port = int(s[1])
-        except ValueError:
-            print "Error: Erroneous port."
-            sys.exit(1)
+# Private messages
+def on_priv_message ( connection, e ):
+    print e.source().split ( '!' ) [ 0 ] + ': ' + e.arguments() [ 0 ]
+    # Respond to a 'hello' message
+    if e.arguments() [ 0 ].lower().find ( 'hello' ) == 0:
+        connection.privmsg ( e.source().split ( '!' ) [ 0 ], 'Hello.' )
     else:
-        port = 6667
-    nickname = sys.argv[2]
-    target = sys.argv[3]
+        try:
+            msg = unicode(e.arguments()[0],'utf-8')
+        except UnicodeDecodeError:
+            connection.privmsg ( e.source().split ( '!' ) [ 0 ], 'Exception: UnicodeDecodeError.' )
+            print "Exception: UnicodeDecodeError"
+            return
 
-    c = IRCCat(target)
+        reputation = user_reputations.get(msg)
+        if(reputation == None): reputation = "User not found. (If user was created before 2010 please report this as a bug.)"
+        connection.privmsg ( e.source().split ( '!' ) [ 0 ], "User %s reputation: %s" % (msg, reputation) )
+        return
+         
+
+# Public messages
+def on_pub_message ( connection, e ):
+    # print e.target() + '> ' + e.source().split ( '!' )[ 0 ] + ': ' + e.arguments() [ 0 ]
+    match = re_edit.match(e.arguments()[0])
+
+    if not match:
+            print "Warning: Regexp does not match (raw): ", e.arguments()[0]
+            return
     try:
-        c.connect(server, port, nickname)
-    except irclib.ServerConnectionError, x:
-        print x
-        sys.exit(1)
-    c.start()
+        msg = unicode(e.arguments()[0],'utf-8')
+    except UnicodeDecodeError:
+        print "Exception: UnicodeDecodeError"
+        return
 
-if __name__ == "__main__":
-    main()
+    match = re_edit.match(msg)
+    if not match:
+        print "Warning: Regexp does not match (unicode)"
+        return
+    
+    d = match.groupdict()
+    reputation = user_reputations.get(d['user'])
+    if(reputation == None or reputation < 0):
+        print d['user'], '(', reputation , ')', d['page'], d['url']
+        c.privmsg("#cvn-wp-en-reputation", ("Reputation %s. " % reputation)  +  e.arguments()[0])
+    return
+
+def on_disconnect(connection, event):
+    print "Error: received on_disconnect"
+    sys.exit(0)
+
+
+if len(sys.argv) != 3:
+    print "Usage: testbot <nickname> reputations.pkl"
+    sys.exit(1)
+
+nickname = sys.argv[1]
+r._reputations_arg = sys.argv[2]
+r._output_arg = None
+user_reputations = r.read_reputations()
+re_edit = re.compile(r'^C14\[\[^C07(?P<page>.+?)^C14\]\]^C4 (?P<flags>.*?)^C10 ^C02(?P<url>.+?)^C ^C5\*^C ^C03(?P<user>.+?)^C ^C5\*^C \(?^B?(?P<bytes>[+-]?\d+?)^B?\) ^C10(?P<summary>.*)^C'.replace('^B', '\002').replace('^C', '\003').replace('^U', '\037'))
+
+# irclib.DEBUG = True
+irc = irclib.IRC()
+try:
+    c = irc.server().connect("irc.freenode.net", 6667, nickname)
+    w = irc.server().connect("irc.wikimedia.org", 6667, nickname)
+    
+except irclib.ServerConnectionError, x:
+    print x
+    sys.exit(1)
+
+irc.add_global_handler("welcome", on_connect)
+irc.add_global_handler('privmsg', on_priv_message )
+irc.add_global_handler('pubmsg', on_pub_message )
+irc.add_global_handler("disconnect", on_disconnect)
+
+try:
+    irc.process_forever()
+    
+except KeyboardInterrupt:
+    c.quit("Ctrl-C at console")
+    w.quit("Ctrl-C at console")
+    print "Quit IRC."
+
+except Exception, e:
+    c.quit("Exception")
+    w.quit("Exception")
+    print("%s: %s" % (e.__class__.__name__, e.args))
+    raise 
+ 
